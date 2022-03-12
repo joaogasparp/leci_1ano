1a09b198b705524d2def5cc8717b49cca37b6cd7
jppgaspar <joaogaspar003@gmail.com>
2022-03-12 22:53:58 +0000

    Ex4

--- 2º SEMESTRE/POO/src/aula02/Ex4.java
+++ 2º SEMESTRE/POO/src/aula02/Ex4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Ex4 {
     public static void main(String[] args) {
 
-        double Minvestido, Jmensal, Total;
+        double Minvestido, Jmensal;
         Scanner input = new Scanner(System.in);
         System.out.print("Insira o montante investido:");
         Minvestido = input.nextDouble();
