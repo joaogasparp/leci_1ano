eda3928dc439e0fd4b46aacf90670dc1aa1f5cf3
jppgaspar <joaogaspar003@gmail.com>
2022-03-12 21:23:18 +0000

    Ex3

---
+++ 2º SEMESTRE/POO/src/aula02/Ex3.java
@@ -0,0 +1,13 @@
+package aula02;
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+
+        double M, finalTemperature, initialTemperature;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira a quantidade de água");
+        sc.close();
+        
+    }
+}
