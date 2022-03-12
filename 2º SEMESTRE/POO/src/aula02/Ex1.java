a8bbd4dd81f0a5e3d698d21d6c3a8d28a86ecce3
jppgaspar <joaogaspar003@gmail.com>
2022-03-12 20:38:28 +0000

    Ex1

---
+++ 2º SEMESTRE/POO/src/aula02/Ex1.java
@@ -0,0 +1,16 @@
+package aula02;
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        double km, miles;
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Insira distância em km: ");
+		km = sc.nextDouble();
+		miles = km / 1.609;
+		System.out.println("A distância em milhas é " + miles);
+		sc.close();
+
+    }
+}
